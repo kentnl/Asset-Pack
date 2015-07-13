17a36d682381cf58998e02ce9ce96e4610023b90
Kent Fredric <kentfredric@gmail.com>
2015-07-13 17:56:03 +1200

    v0.000003
     [BugFix]
     - Don't execute FatPacker interop test with FatPacker < 0.00917
     - Fatpacking itself should still work before this, but its just not practical to test
    
     [Dependencies::Stats]
     - Dependencies changed since 0.000002, see misc/*.deps* for details
     - test: (recommends: ↑1)

--- lib/Asset/Pack.pm
+++ lib/Asset/Pack.pm
@@ -7,7 +7,7 @@ package Asset::Pack;
 use Path::Tiny 0.069 qw( path );    # path()->visit without broken ref returns
 use Try::Tiny qw( try catch );
 
-our $VERSION = '0.000003';
+our $VERSION = '0.000004';
 
 # ABSTRACT: Easily pack assets into Perl Modules that can be fat-packed
 
