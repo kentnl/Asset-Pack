3d22e882ddfcef42873697f71e54ecf765134e33
Kent Fredric <kentfredric@gmail.com>
2017-03-08 00:04:57 +1300

    Build results of 5321851 (on master)

--- lib/Asset/Pack.pm
+++ lib/Asset/Pack.pm
@@ -569,7 +569,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by James Laver on time generously donated by Anomalio.
+This software is copyright (c) 2017 by James Laver on time generously donated by Anomalio.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
